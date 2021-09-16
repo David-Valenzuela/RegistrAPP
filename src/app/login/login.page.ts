1f6db2453811a3c809f0b33148d4b76d300c9a75
David Valenzuela <dav.valenzuelat@duocuc.cl>
2021-09-16 17:26:53 -0300

    Autentificación del Usuario

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router} from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
-
-  constructor() { }
+  usuario : String;
+  clave : String;
+  respuesta: String;
+  constructor(private router: Router) { 
+    this.usuario = "Juan";
+    this.clave = "juanduoc2021";
+  }
 
   ngOnInit() {
+  }
+  
+  ingresar(user : HTMLInputElement, pass : HTMLInputElement)
+  {
+    let nombre = user.value
+    let password = pass.value
+    if(nombre == this.usuario && password == this.clave){
+      console.log("Datos Correctos")
+      this.router.navigate(['/home']);
+    } else{
+      console.log("Datos Incorrectos")
+      this.respuesta = "Ingrese nuevamente los datos."
+    }
   }
 
 }
